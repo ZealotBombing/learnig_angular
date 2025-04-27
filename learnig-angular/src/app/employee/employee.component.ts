e274a87475320eed353eebb509ce4115dd6a6376
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2025-04-27 03:13:16 -0400

    back to correct way

--- learnig-angular/src/app/employee/employee.component.ts
+++ learnig-angular/src/app/employee/employee.component.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-employee',
   imports: [],
-  // templateUrl: './employee.component.html',
-  template: "<p>Fernanda Piña</p>",
-  // styleUrl: './employee.component.css'
-  styles: ["p{background-color:red}"]
+  templateUrl: './employee.component.html',
+  // template: "<p>Fernanda Piña</p>",
+  styleUrl: './employee.component.css'
+  // styles: ["p{background-color:red}"]
 })
 export class EmployeeComponent {
 
